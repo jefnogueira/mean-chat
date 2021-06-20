6eaee6d5ff7ed899b5d1cd5460aee9f7e7dba070
jeferson <jefersonnogueira95@yahoo.com>
2021-06-20 20:24:36 -0300

    Primeiro commit

---
+++ backend/src/config.ts
@@ -0,0 +1,9 @@
+class Config {
+    public SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+    public connectionString = 'mongodb+srv://jefnogueira:aparecida@cluster0.9p3sj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
+
+export default new Config();
+
+
+//'mongodb+srv://andrew:meanChat@cluster0-42jwz.mongodb.net/test?retryWrites=true&w=majority';
